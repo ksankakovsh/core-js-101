63e0d888e0161e42ca1c29b089d70e631eb5974c
Ksanka Kovsh <ksanka.kovsh@gmail.com>
2023-01-02 23:46:48 +0300

    feat add solutions for 03 , 07

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -98,8 +98,10 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  if (a < (b + c) && b < (c + a) && c < (b + a)) {
+    return true;
+  } return false;
 }
 
 
